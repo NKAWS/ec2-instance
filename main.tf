321f0c76e40ded49362a20c75f1fe6884051adb9
Naresh kumar <naresh.kesa19@gmail.com>
2023-09-16 14:02:41 +0530

    timing vpc

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "aws_vpc" "main" {
   enable_dns_support = true
   enable_dns_hostnames = true
   tags = {
-    Name = "main"
+    Name = "timing"
     Terraform = "true"
     Environment = "Dev"
   }
