b4c64a92eba183176145e443d5520be974988974
Naresh kumar <naresh.kesa19@gmail.com>
2023-09-16 16:56:05 +0530

    NAT gw

--- main.tf
+++ main.tf
@@ -92,3 +92,25 @@ resource "aws_route_table_association" "database" {
   subnet_id      = aws_subnet.database_subnet.id
   route_table_id = aws_route_table.database_route_table.id
 }
+resource "aws_eip" "nat" {
+  domain   = "vpc"
+}
+resource "aws_nat_gateway" "gw" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_subnet.id
+}
+  tags = {
+    Name = "timing NAT gw"
+  }
+resource "aws_route" "private" {
+  route_table_id            = aws_route_table.private_route_table.id
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id            = aws_nat_gateway.gw.id
+  #depends_on                = [aws_route_table.private]
+}
+resource "aws_route" "database" {
+  route_table_id            = aws_route_table.database_route_table.id
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id            = aws_nat_gateway.gw.id
+  #depends_on                = [aws_route_table.private]
+}
