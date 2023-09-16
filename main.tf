fa6da7ae4c7748286c8bbae773b9aebf42aebe12
Naresh kumar <naresh.kesa19@gmail.com>
2023-09-16 16:33:44 +0530

    association

--- main.tf
+++ main.tf
@@ -23,11 +23,72 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public_subnet" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
+  tags       = {
+    Name        = "timing-public-subnet"
+    Terraform   = "true"
+    Environment = "Dev"
+  }
+}
+
+  resource "aws_route_table" "public_route_table" {
+    vpc_id = aws_vpc.main.id
+
+    route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.main.id
+    }
+
+    tags = {
+      Name = "timing-public-rt"
+      Terraform = "true"
+      Environment = "Dev"
+    }
+  }
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.11.0/24"
+  tags       = {
+    Name        = "timing-private-subnet"
+    Terraform   = "true"
+    Environment = "Dev"
+  }
+}
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.main.id
+
   tags = {
-    Name = "timing-public-subnet"
+    Name = "timing-public-rt"
     Terraform = "true"
     Environment = "Dev"
   }
-
 }
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+resource "aws_subnet" "database_subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.21.0/24"
+  tags       = {
+    Name        = "timing-database-subnet"
+    Terraform   = "true"
+    Environment = "Dev"
+  }
+}
+resource "aws_route_table" "database_route_table" {
+  vpc_id = aws_vpc.main.id
 
+  tags = {
+    Name = "timing-database-rt"
+    Terraform = "true"
+    Environment = "Dev"
+  }
+}
+resource "aws_route_table_association" "database" {
+  subnet_id      = aws_subnet.database_subnet.id
+  route_table_id = aws_route_table.database_route_table.id
+}
